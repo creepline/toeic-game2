f93dd162f03aa2545784ad9b434236e25058624d
ToeicGame <toeic-game@local.com>
2026-03-21 12:54:18 +0800

    Remove files with secrets

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
-  build: {
-    assetsDir: 'assets',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vue-vendor': ['vue', 'element-plus'],
-          'icons': ['@element-plus/icons-vue']
-        }
-      }
-    }
-  }
+  base: './'
 })
